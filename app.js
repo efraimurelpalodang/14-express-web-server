bafd2c339c6802ef6f8861806a6cf05721c387ea
efraim urel <efraimurelpalodang@gmail.com>
2024-11-13 15:28:15 +0800

    menggunakan req.params dan req.query untuk requestnya

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+app.get('/product/:id', (req,res) => {
+  res.send(`Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`);
+});
+
 app.use('/', (req,res) => {
   res.status(404);
   res.send('<h1>404</h1>')
